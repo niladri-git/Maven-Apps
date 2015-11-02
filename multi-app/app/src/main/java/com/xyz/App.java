8b14ac7d9ff864b73b2c56259b28904bbc0bca5e
Niladri Datta <niladridatta@gmail.com>
2015-11-02 23:47:22 +0530

    Created Multi Module Maven Project and deployed artifacts to local File System

---
+++ multi-app/app/src/main/java/com/xyz/App.java
@@ -0,0 +1,11 @@
+package com.xyz;
+
+import com.xyz.util.*;
+
+public class App {
+
+        public static void main(String[] args) {
+                System.out.println("Hello World! Today is " + DateUtil.getToday());
+        }
+
+}
---
+++ multi-app/util/src/main/java/com/xyz/util/DateUtil.java
@@ -0,0 +1,12 @@
+package com.xyz.util;
+
+import java.util.Date;
+import org.apache.commons.lang.time.DateFormatUtils;
+
+public class DateUtil {
+
+        public static String getToday() {
+                String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
+                return today;
+        }
+}
